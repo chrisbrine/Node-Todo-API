c35f42da56ebd2097d234b0f33cea5b05272b809
Chris Brine <chrisbrine.web@gmail.com>
2018-11-20 17:08:03 -0800

    Updated server and server.test
    Added 404 for IDs not found with GET and also added test for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -37,6 +37,10 @@ app.get('/todos/:id', (req, res) => {
   }
 
   Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send({ error: 'ID not found' });
+    }
+
     res.send({ todo });
   }, (e) => {
     res.status(400).send(e);
