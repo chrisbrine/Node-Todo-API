4626c79732ae120ea38a34b3ff9e9e757f31735c
Chris Brine <chrisbrine.web@gmail.com>
2018-11-20 17:16:24 -0800

    Updated package.json and server.js
    Added port to package.json and port to server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ const { mongoose }  = require('./db/mongoose');
 const { Todo }      = require('./models/todo');
 const { User }      = require('./models/user');
 
-var app = express();
+const port = process.env.PORT || 3000;
+
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -47,8 +49,8 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000.');
+app.listen(port, () => {
+  console.log(`Started on port ${port}.`);
 });
 
 module.exports = { app };
